84b724c84a384e8075cd09c547de21543e6f8605
AlexKT1022 <alexkt1022@gmail.com>
2025-04-28 14:09:05 -0400

    added basic SideNav component code

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,20 @@
-const SideNav = () => {
-  return <nav>SideNav</nav>;
+import { first151Pokemon, getFullPokedexNumber } from '../utils';
+
+const SideNav = ({ selectedPokemon, setSelectedPokemon }) => {
+  return (
+    <nav>
+      <div className='header'>
+        <h1 className='text-gradient'>Pokédex</h1>
+      </div>
+      <input type='text' />
+      {first151Pokemon.map((pokemon, pokemonIndex) => (
+        <button key={pokemonIndex} className={'nav-card'}>
+          <p>{getFullPokedexNumber(pokemonIndex)}</p>
+          <p>{pokemon}</p>
+        </button>
+      ))}
+    </nav>
+  );
 };
 
 export default SideNav;
