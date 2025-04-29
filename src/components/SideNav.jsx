b394c50b340630e6d33dcad4dae82b6ba0163d06
AlexKT1022 <alexkt1022@gmail.com>
2025-04-29 14:10:37 -0400

    added search functionality

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,26 +1,57 @@
+import { useState } from 'react';
 import { first151Pokemon, getFullPokedexNumber } from '../utils';
 
 const SideNav = ({ selectedPokemon, setSelectedPokemon }) => {
+  const [searchValue, setSearchValue] = useState('');
+  const filteredPokemon = first151Pokemon.filter(
+    (pokemonName, pokemonIndex) => {
+      if (getFullPokedexNumber(pokemonIndex).includes(searchValue)) {
+        return true;
+      }
+
+      if (pokemonName.toLowerCase().includes(searchValue)) {
+        return true;
+      }
+
+      return false;
+    }
+  );
+
+  const handleSearchInput = (e) => {
+    setSearchValue(e.target.value);
+  };
+
   return (
     <nav>
       <div className='header'>
         <h1 className='text-gradient'>Pokédex</h1>
       </div>
-      <input type='text' />
-      {first151Pokemon.map((pokemon, pokemonIndex) => (
-        <button
-          key={pokemonIndex}
-          className={`${
-            pokemonIndex === selectedPokemon
-              ? 'nav-card nav-card-selected'
-              : 'nav-card'
-          }`}
-          onClick={() => setSelectedPokemon(pokemonIndex)}
-        >
-          <p>{getFullPokedexNumber(pokemonIndex)}</p>
-          <p>{pokemon}</p>
-        </button>
-      ))}
+      <input
+        type='text'
+        value={searchValue}
+        placeholder='Search'
+        onChange={(e) => {
+          handleSearchInput(e);
+        }}
+      />
+      {filteredPokemon.map((pokemon, pokemonIndex) => {
+        const pokedexNumber = first151Pokemon.indexOf(pokemon);
+
+        return (
+          <button
+            key={pokemonIndex}
+            className={`${
+              pokemonIndex === selectedPokemon
+                ? 'nav-card nav-card-selected'
+                : 'nav-card'
+            }`}
+            onClick={() => setSelectedPokemon(pokedexNumber)}
+          >
+            <p>{getFullPokedexNumber(pokedexNumber)}</p>
+            <p>{pokemon}</p>
+          </button>
+        );
+      })}
     </nav>
   );
 };
